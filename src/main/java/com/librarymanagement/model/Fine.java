3a0feb71bf5e762048c8f6853210492a468cb75b
HuyDang05 <quanghuy23052005@gmail.com>
2024-10-12 22:08:11 +0700

    next
    ...

---
+++ src/main/java/com/librarymanagement/model/Fine.java
@@ -0,0 +1,20 @@
+package com.librarymanagement.model;
+import java.util.Date;
+
+//Quan ly cac khoan phat khi nguoi dung muon qua han
+public class Fine {
+    private int fineId;
+    private User user;
+    private double amount;
+    private Date lateDate;
+
+    //Tinh toan phi phat dua tren so ngay qua han
+    public /*return_type*/ calculateFine() {
+        //To do
+    }
+
+    //Thanh toan phi phat
+    public /*return_type*/ payFine() {
+        //To do
+    }
+}
---
+++ src/main/java/com/librarymanagement/model/Notification.java
@@ -0,0 +1,15 @@
+package com.librarymanagement.model;
+
+//Quan ly thong bao cho nguoi dung ve tai lieu hoac thong tin muon/tra
+public class Notification {
+    private int notificationId;
+    private User user;
+    private String message;
+
+    //Gui thong bao cho nguoi dung
+    public /*return_type*/ sendNotification() {
+        //To do
+    }
+
+
+}
