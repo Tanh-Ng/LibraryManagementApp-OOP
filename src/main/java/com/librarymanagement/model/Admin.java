39186430f44da22fd5c16ce3ea8450b77d54ed2b
HuyDang05 <quanghuy23052005@gmail.com>
2024-10-12 21:33:57 +0700

    Next
    ...

--- src/main/java/com/librarymanagement/model/Admin.java
+++ src/main/java/com/librarymanagement/model/Admin.java
@@ -1,4 +1,15 @@
 package com.librarymanagement.model;
 
 public class Admin {
+    public /*return_type*/ addDocument(Document doc) {
+        //To do
+    }
+
+    public /*return_type*/ removeDocument(Document doc) {
+        //To do
+    }
+
+    public /*return_type*/ updateDocument(Document doc) {
+        //To do
+    }
 }
--- src/main/java/com/librarymanagement/model/Library.java
+++ src/main/java/com/librarymanagement/model/Library.java
@@ -1,5 +1,31 @@
 package com.librarymanagement.model;
 
+import java.util.List;
+
 public class Library {
+    private List<Document> documents;
+    private List<User> users;
+
+    public /*return_type*/ addDocument(Document doc, Admin ad) {
+        //To do
+    }
+
+    public /*return_type*/ removeDocument(Document doc, Admin ad) {
+        //To do
+    }
+
+    public /*return_type*/ UpdateDocument(Document doc, Admin ad) {
+        //To do
+    }
+
+    public /*return_type*/ addDocument(User user, Admin ad) {
+        //To do
+    }
+
+    public /*return_type*/ addDocument(User user, Admin ad) {
+        //To do
+    }
+
+
 
 }
