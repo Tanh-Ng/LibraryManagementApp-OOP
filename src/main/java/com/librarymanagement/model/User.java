58be641f8535af934e1cb10cbecb5359e51c3ba9
Tanh-Ng <annhnguyen0412@gmail.com>
2024-10-14 15:57:24 +0700

    add javaDoc to declare function

--- src/main/java/com/librarymanagement/model/User.java
+++ src/main/java/com/librarymanagement/model/User.java
@@ -2,25 +2,50 @@ package com.librarymanagement.model;
 
 import java.util.List;
 
+/**
+ * Represents a user in the library.
+ */
 public class User {
     private int userId;
     private String name;
     private List<Document> borrowedDocuments;
 
-    public /*return_type*/ borrowDocument(Document doc) {
+    /**
+     * Borrows a document.
+     *
+     * @param document The document that the user wants to borrow.
+     */
+    public void borrowDocument(Document document) {
         //To do
     }
 
-    public /*return_type*/ returnDocument(Document doc) {
+    /**
+     * Returns a borrowed document.
+     *
+     * @param document The document that the user wants to return.
+     */
+    public void returnDocument(Document document) {
         //To do
     }
 
-    public /*return_type*/ setPassword(String password) {
+    /**
+     * Sets a new password for the user.
+     *
+     * @param password The new password that the user wants to set.
+     */
+    public void setPassword(String password) {
         //To do
     }
 
+    /**
+     * Validates the user's password
+     *
+     * @param password the password to be validated.
+     * @return true if the password is valid, false otherwise.
+     */
     public boolean validatePassword(String password) {
         //To do
+        return false;
     }
 
 }
